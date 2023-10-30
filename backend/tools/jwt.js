2bb44a543f4622503ef58621e8583c3119bb7c2f
blatisgense <lavr.marudenko@gmail.com>
2023-10-30 13:35:49 +0300

    backup

--- backend/tools/jwt.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const JWT_tokens = ({user_name, user_email, user_role, user_wheel, user_money, user_meditation, user_list}) => {
-    const user = {user_name,  user_email, user_role, user_wheel, user_money, user_meditation, user_list};
-    const access_token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'15m'});
-    const refresh_token = jwt.sign(user, process.env.UPDATE_TOKEN_SECRET, {expiresIn:'36h'});
-    return ({access_token, refresh_token});
-}
-export {JWT_tokens};
